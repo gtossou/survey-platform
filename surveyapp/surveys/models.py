6d187630f5212dbd1d26c78585966c677d370c26
gtossou <gtossou@users.noreply.github.com>
2022-11-18 01:13:28 +0100

    Revert "from coding session today, also added a gitignore"

--- surveyapp/surveys/models.py
+++ surveyapp/surveys/models.py
@@ -1,28 +1,29 @@
-from django.contrib.auth.models import User
 from django.db import models
+import uuid
 
 
 class Survey(models.Model):
     title = models.CharField(max_length=400)
     description = models.TextField(null=True)
-    link = models.URLField(max_length=500)
-    created = models.DateTimeField(auto_now_add=True)
+    link = models.CharField()
+    created = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
+
+# class User
 
 class Question(models.Model):
     title = models.TextField(null=True)
-    # TODO: make choicelist to have a fixed set of types
-    type = models.CharField(null=True, max_length=20)
+    type = models.CharField(null=True)
     is_mandatory = models.BooleanField(default=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
 
 
 class Answer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    # orm create new row created gets set automatically = when question gets
-    # opened
-    created = models.DateTimeField(auto_now_add=True)
-    # orm upon update (answering question), set content AND answered datetime
     content = models.TextField()
-    answered = models.DateTimeField(null=True, blank=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    users = models.ManyToManyField(User)
+    questions = models.ManyToManyField(Question)
