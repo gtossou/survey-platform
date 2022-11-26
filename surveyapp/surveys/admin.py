bf0da112f57e223b5627c69464a37592101c42ce
gtossou <gil.tossou@gmail.com>
2022-11-27 00:23:30 +0100

    register admin model

--- surveyapp/surveys/admin.py
+++ surveyapp/surveys/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, QuestionItem
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in
+                    Question._meta.get_fields()]
+
+
+@admin.register(QuestionItem)
+class QuestionItemAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in
+                    QuestionItem._meta.get_fields()]
--- surveyapp/surveys/models.py
+++ surveyapp/surveys/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 
 from django.db import models
-import jsonfield
 
 
 class Survey(models.Model):
@@ -24,26 +23,34 @@ class Question(models.Model):
     type = models.CharField(
         max_length=32,
         choices=QUESTION_TYPES,
-        default=SHORT_ANSWER,
+        # TODO : use values instead of number
+        default=QUESTION_TYPES[0],
     )
     is_mandatory = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class QuestionItem(models.Model):
     isShortAnswer = models.BooleanField(default=False)
-    short_answer_content = models.CharField()
+    short_answer_content = models.CharField(max_length=400)
     isParagraph = models.BooleanField(default=False)
-    paragraph_content = models.CharField()
+    paragraph_content = models.CharField(max_length=3000)
     isRadio = models.BooleanField(default=False)
-    radio_content = jsonfield.JSONField("RadioData", default={})
+    radio_content = models.JSONField("RadioData", default=dict)
     isCheckbox = models.BooleanField(default=False)
-    checkbox_content = jsonfield.JSONField("CheckboxData", default={})
+    checkbox_content = models.JSONField("CheckboxData", default=dict)
     isDropdown = models.BooleanField(default=False)
     # TODO: Field to store list?
-    dropdown_content = models.CharField()
+    dropdown_content = models.CharField(max_length=1200)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.isShortAnswer
 
 
 class Answer(models.Model):
@@ -55,3 +62,8 @@ class Answer(models.Model):
     # orm upon update (answering question), set content AND answered datetime
     content = models.TextField()
     answered = models.DateTimeField(null=True, blank=True)
+
+    # TODO: Implement logic jump
+
+    # TODO: You can assign any JSON-encodable object to this field. It will be JSON-encoded before being stored in the database as a text value and it will be turned back into a python list/dict/string upon retrieval from the database.
+    # There is also a TypedJSONField, that allows you to define data types that must be included within each object in the array. More documentation to follow
