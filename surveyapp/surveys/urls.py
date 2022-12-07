95ed4e8efed0a463ceaf57abd038fe81bf1f3c11
gtossou <gil.tossou@gmail.com>
2022-12-07 21:44:59 +0100

    add survey list links

--- surveyapp/surveys/urls.py
+++ surveyapp/surveys/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import CreateSurveyView
+from .views import createSurveyView, surveyListView, renderSurveyView
 
 urlpatterns = [
     # TODO : set default template
     # path("create_survey", CreateSurveyView.as_view(), name="create survey")
-    path("create_survey", CreateSurveyView, name="create survey")
+    path("create_survey", createSurveyView, name="create survey"),
+    path("survey_list", surveyListView.as_view(), name="survey list"),
+    path("render_survey/<str:survey_link>",
+         renderSurveyView, name="render survey")
 ]
